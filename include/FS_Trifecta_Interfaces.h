12afc5289a8af888d88a1e79b31c7112ef423ff0
Triangle Man <trianglemanproductions@gmail.com>
2025-04-06 01:16:09 -0700

    Improved the stability and function of UDP connections in support of multi-device connections

--- include/FS_Trifecta_Interfaces.h
+++ include/FS_Trifecta_Interfaces.h
@@ -79,7 +79,8 @@ extern "C"
     ssize_t fs_transmit_serial(fs_device_info *device_handle, void *tx_buffer, size_t length_bytes, int timeout_micros);
 
     /// @brief Receives data via TCP from the specified device.
-    /// @param device_handle Pointer to the device information structure.
+    /// @param device_list Pointer to the list of connected devices.
+    /// @param device_handle Pointer to the device that received the message.
     /// @param rx_buffer Pointer to the buffer to store received data.
     /// @param length_bytes Number of bytes to receive.
     /// @param timeout_micros Timeout for the operation in microseconds.
@@ -87,7 +88,8 @@ extern "C"
     ssize_t fs_receive_networked_tcp(fs_device_info *device_handle, void *rx_buffer, size_t length_bytes, int timeout_micros);
 
     /// @brief Receives data via UDP from the specified device.
-    /// @param device_handle Pointer to the device information structure.
+    /// @param device_list Pointer to the list of connected devices.
+    /// @param device_handle Pointer to the device that received the message.
     /// @param rx_buffer Pointer to the buffer to store received data.
     /// @param length_bytes Number of bytes to receive.
     /// @param timeout_micros Timeout for the operation in microseconds.
