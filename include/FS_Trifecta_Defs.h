b5338efb3c288c5aedf85d077c3b98d592ce1f38
Sgushy <pointpointy@gmail.com>
2025-09-18 20:38:24 -0700

    fs_platform_supported_serial_interrupts() to return OR flag of supported serial interrupt interfaces

--- include/FS_Trifecta_Defs.h
+++ include/FS_Trifecta_Defs.h
@@ -56,7 +56,7 @@ extern "C"
     } fs_run_status_t;
 
     typedef struct fs_driver_config
-    {        
+    {
         bool use_serial_interrupt_mode;    // If TRUE, and the platform supports it, then use serial interrupt mode intead
         int background_task_priority;      // Priority of the background task for obtaining updates from the device (leave as -1 if no preference)
         int background_task_core_affinity; // Core to pin the background task to (leave as -1 if no preference)
@@ -114,13 +114,14 @@ extern "C"
         fs_communication_mode_t communication_mode; // Communication mode
         int32_t ping;                               // Time since last received communication from device
 
-        fs_driver_config_t driver_config; // Device driver configuration (each device has its own thread spawned)
-        fs_run_status_t status;           // 0 = UNINITIALIZED/STOPPED, 1 = RUNNING, -1 = ERROR
+        fs_driver_config_t driver_config; // Device driver configuration (each device has its own thread spawned unless interrupt mode is active)
+        uint64_t hp_timestamp_micros;     // If serial interrupt mode is enabled, this enables accurate timestamping of most recent packet.
 
-        char ip_addr[39]; // If networking is used, this is the corresponding IP address string
-        int ip_port;      // Always 8888
-        int tcp_sock;     // TCP socket number
-        int udp_sock;     // UDP socket number
+        fs_run_status_t status; // 0 = UNINITIALIZED/STOPPED, 1 = RUNNING, -1 = ERROR
+        char ip_addr[39];       // If networking is used, this is the corresponding IP address string
+        int ip_port;            // Always 8888
+        int tcp_sock;           // TCP socket number
+        int udp_sock;           // UDP socket number
 
         int serial_port;                         // If serial communication of some kind is used, this is the corresponding port
         int32_t baudrate;                        // If serial communication is used, baud rate. NOTE: USB-CDC ignores this completely
@@ -129,33 +130,34 @@ extern "C"
         fs_command_ringbuffer_t command_queue;   // Command buffer for the device (read-only)
     } fs_device_info_t;
 
-#define FS_DEVICE_INFO_UNINITIALIZED {                                                  \
-    .device_name = {0},                                        /* device_name */        \
-    .device_id = 0,                                            /* device_id */          \
-    .communication_mode = FS_COMMUNICATION_MODE_UNINITIALIZED, /* communication_mode */ \
-    .ping = 9999,                                              /* ping */               \
-    .driver_config = FS_DRIVER_CONFIG_DEFAULT,                 /* driver_config */      \
-    .status = FS_RUN_STATUS_IDLE,                              /* status */             \
-    .ip_addr = {0},                                            /* ip_addr */            \
-    .ip_port = 8888,                                           /* ip_port */            \
-    .tcp_sock = -1,                                            /* tcp_sock */           \
-    .udp_sock = -1,                                            /* udp_sock */           \
-    .serial_port = -1,                                         /* serial_port */        \
-    .baudrate = 0,                                             /* baudrate */           \
-    .data_buffer = {                                           /* data_buffer */        \
-                    .buffer = {0},                                                      \
-                    .head = 0,                                                          \
-                    .tail = 0,                                                          \
-                    .count = 0},                                                        \
-    .packet_buf_queue = {/* packet_buf_queue */                                         \
-                         .buffer = {0},                                                 \
-                         .head = 0,                                                     \
-                         .tail = 0,                                                     \
-                         .count = 0},                                                   \
-    .command_queue = {/* command_queue */                                               \
-                      .buffer = {0},                                                    \
-                      .head = 0,                                                        \
-                      .tail = 0,                                                        \
+#define FS_DEVICE_INFO_UNINITIALIZED {                                                        \
+    .device_name = {0},                                        /* device_name */              \
+    .device_id = 0,                                            /* device_id */                \
+    .communication_mode = FS_COMMUNICATION_MODE_UNINITIALIZED, /* communication_mode */       \
+    .ping = 9999,                                              /* ping */                     \
+    .driver_config = FS_DRIVER_CONFIG_DEFAULT,                 /* driver_config */            \
+    .hp_timestamp_micros = 0,                                  /* High precision timestamp */ \
+    .status = FS_RUN_STATUS_IDLE,                              /* status */                   \
+    .ip_addr = {0},                                            /* ip_addr */                  \
+    .ip_port = 8888,                                           /* ip_port */                  \
+    .tcp_sock = -1,                                            /* tcp_sock */                 \
+    .udp_sock = -1,                                            /* udp_sock */                 \
+    .serial_port = -1,                                         /* serial_port */              \
+    .baudrate = 0,                                             /* baudrate */                 \
+    .data_buffer = {                                           /* data_buffer */              \
+                    .buffer = {0},                                                            \
+                    .head = 0,                                                                \
+                    .tail = 0,                                                                \
+                    .count = 0},                                                              \
+    .packet_buf_queue = {/* packet_buf_queue */                                               \
+                         .buffer = {0},                                                       \
+                         .head = 0,                                                           \
+                         .tail = 0,                                                           \
+                         .count = 0},                                                         \
+    .command_queue = {/* command_queue */                                                     \
+                      .buffer = {0},                                                          \
+                      .head = 0,                                                              \
+                      .tail = 0,                                                              \
                       .count = 0}}
 
 #ifdef __cplusplus
--- include/FS_Trifecta_Interfaces.h
+++ include/FS_Trifecta_Interfaces.h
@@ -39,13 +39,14 @@ extern "C"
 
     /// @brief Whether interrupt-driven UART etc. is supported by the platform.
     /// Many RTOSes support this, but Linux does not, etc.
-    /// @return TRUE if so, FALSE otherwise.
-    bool fs_platform_supports_serial_interrupts();
+    /// @return OR FLAG of serial interfaces which support interrupts.
+    /// If the return is FS_COMMUNICATION_MODE_UNINITIALIZED, then no interrupt-driven serial is allowed.
+    int fs_platform_supported_serial_interrupts();
 
-    /// @brief Start serial in interrupt mode on platforms that support it. 
+    /// @brief Start serial in interrupt mode on platforms that support it.
     /// This enables more precise and low latency serial reads than polling.
-    /// @param device_handle 
-    /// @param status_flag 
+    /// @param device_handle
+    /// @param status_flag
     /// @return 0 on success, -1 on fail (e.g. not supported on platform)
     int fs_init_serial_interrupts(fs_device_info_t *device_handle, fs_run_status_t *status_flag);
 
