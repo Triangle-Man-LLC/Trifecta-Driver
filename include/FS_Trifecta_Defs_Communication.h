3ceb5f149c8c6a916f782c2bbdb038598420cb6e
Triangle Man <trianglemanproductions@gmail.com>
2025-12-15 14:50:00 -0800

    Add preliminary support for Trifecta-K0 and Trifecta-M devices

--- include/FS_Trifecta_Defs_Communication.h
+++ include/FS_Trifecta_Defs_Communication.h
@@ -41,40 +41,47 @@ extern "C"
     /// @brief Command definitions
     typedef enum
     {
+        PACKET_HEADER = ':',       // The packet header indicates the start of a packet
+        PACKET_FOOTER = '!',       // The packet footer indicates the end of a packet
+        CMD_TERMINATOR = ';',      // The command terminator denotes the end of a command.
         CMD_RESTART = 'R',         // Restart the device "R0;"
         CMD_CLEAR_CONFIG = 'C',    // Clear all saved settings "C0;"
         CMD_SET_SSID = 'S',        // Set SSID for WiFi connection (UART only) "S<STA_SSID>;"
         CMD_SET_PASSWORD = 'P',    // Set password for WiFi connection (UART only) "P<STA_PASSWORD>;"
         CMD_SET_SSID_AP = 'q',     // Set SSID for access point WiFi connection (UART only) "q<AP_SSID>;"
         CMD_SET_PASSWORD_AP = 'w', // Set password for access point WiFi connection (UART only) "w<AP_PASSWORD>"
-        CMD_SET_FILT_BETA = 'B',   // Set the beta of the orientation filter ? Unused?
         CMD_SET_DEV_NAME = 'N',    // Set device name "N<DEVICE_NAME>;"
-        CMD_SETUP_FINISH = 'F',    // Exit setup and begin telemetry mode "F0;"
+        CMD_SETUP_FINISH = 'F',    // Restart the device following setup "F0;"
 
         CMD_IDENTIFY = 'I',                      // Request identification (device name) "I0;"
-        CMD_IDENTIFY_PARAM_DEV_SN = 'p',         // Respond with device serial number (will be set by eFuse on production model) "p0;"
+        CMD_IDENTIFY_PARAM_DEV_SN = 'p',         // Respond with device serial number "p0;"
         CMD_IDENTIFY_PARAM_DEVMODEL = 'm',       // Respond with device model name "m0;"
         CMD_IDENTIFY_PARAM_DEVFWVERSION = 'f',   // Respond with device firmware version "f0;"
         CMD_IDENTIFY_PARAM_DEVDESC = 'd',        // Respond with device description "d0;"
-        CMD_IDENTIFY_PARAM_ACCELRANGE = 'a',     // Respond with max acceleration range "a0;"
-        CMD_IDENTIFY_PARAM_GYRORANGE = 'g',      // Respond with max gyro range "g0;"
-        CMD_IDENTIFY_PARAM_REFRESHRATE = 'r',    // Respond with refresh rate (typically 200 Hz) "r0;"
         CMD_IDENTIFY_PARAM_UART_BAUD_RATE = 'b', // Respond with UART baud rate "b0;"
         CMD_IDENTIFY_PARAM_SSID = 's',           // Respond with current SSID (STA) for WiFi connection "s0;"
-        CMD_IDENTIFY_PARAM_SSID_AP = '1',        // Respond with current SSID (AP) for WiFi connection "l0;"
+        CMD_IDENTIFY_PARAM_SSID_AP = '1',        // Respond with current SSID (AP) for WiFi connection "10;"
         CMD_IDENTIFY_PARAM_PASSWORD_AP = '3',    // Respond with current SSID (AP) for WiFi connection "30;"
         CMD_IDENTIFY_PARAM_TRANSMIT = 't',       // Respond with transmit mode (serial/UDP/etc.) "t-1;" to query, "t<COMMUNICATION_MODE_1 | 2 | ... |>;" to set
 
         CMD_REZERO_IMUS = 'Z',            // Re-calibrate the IMUs (should only do on a flat plane and stationary) "Z<NUM_CALIBRATION_POINTS>;"
-        CMD_TOGGLE_REZERO_AT_START = 'K', // Toggle re-zeroing IMU at device reboot - this rezero affects accelerometers "K<1 == DO NOT ELSE 0>;"
 
-        CMD_REZERO_INS = '0',  // Reset INS position to zero "00;"
+        CMD_SET_POSITION = '0',  // Reset INS position: "0<type>,<X>,<Y>,<Z>;" where <type> is a fs_gnss_position_format_t
+        CMD_SET_ORIENTATION_OFFSET = 'Q', // Sets the offset orientation: "Q0;" to clear, "Q1;" to use current orientation, else "Q<W>,<X>,<Y>,<Z>;"
         CMD_SET_YAW_DEG = 'y', // Set yaw angle to the given argument (degrees) "y<DEG>;"
 
         CMD_STREAM = 'A',             // Start or stop streaming data "A<0 == STOP, 1 == STREAM, 2 == ONE SHOT READ>;"
         CMD_SET_LISTENING_PORT = 'l', // Set the target port for UDP listener on host device (default: 8888) "l<PORT 1024-65535>;"
-    } fs_command_t;
 
+        CMD_OTA = '@', // Signal start of OTA - upon receiving the command "@1;", the device will begin waiting for OTA to commence.
+                       // Upon receiving the command "@0;", the device will end OTA update.
+                       // It will then validate the OTA data and restart the system.
+                       // If the OTA was bad, it will revert to factory firmware or previous known working firmware.
+                       // OTA sources can be received from UART, USB, or TCP.
+
+        CMD_DISPLAYMODE = 'D' // Set display mode (0 = ACCEL, 1 = GYRO, etc.) "D<MODE 0-9>;"
+    } fs_command_t;
+    
     /// @brief Communication mode between the device and the host (this machine)
     typedef enum fs_communication_mode
     {
